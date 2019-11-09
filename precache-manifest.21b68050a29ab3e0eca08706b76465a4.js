c0949a02e717078fde6bd9555a760c93f755ad1f
Fachri Naufal <fachrinfl@gmail.com>
2019-11-09 16:04:05 +0700

    Updates

---
+++ precache-manifest.21b68050a29ab3e0eca08706b76465a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15eb43431f9c9ea6b30638e1a23f03a4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "67e1b3af6c4c4b151ed8",
+    "url": "/monsters-rolodex/static/css/main.b47f7e6b.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "67e1b3af6c4c4b151ed8",
+    "url": "/monsters-rolodex/static/js/main.648b91fc.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
